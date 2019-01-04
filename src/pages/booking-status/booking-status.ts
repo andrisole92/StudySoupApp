dc2851ca0a7d92d5cedd154c4eeb0e424ff4ffb5
andrisole92 <andrisole92@interstruct.com>
2019-01-03 19:04:07 -0500

    updated input

--- src/pages/booking-status/booking-status.ts
+++ src/pages/booking-status/booking-status.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams, Platform} from 'ionic-angular';
 import {LaunchNavigator, LaunchNavigatorOptions} from "../../../../Testr/node_modules/@ionic-native/launch-navigator";
 
 @Component({
@@ -12,28 +12,36 @@ export class BookingStatusPage {
   ava = "assets/imgs/icon.png";
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              private launchNavigator: LaunchNavigator) {
+              private launchNavigator: LaunchNavigator,
+              private plt: Platform) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RequestStatusPage');
   }
 
-  updatedInfo(){
-    if (this.navCtrl.canGoBack()){
+  updatedInfo() {
+    if (this.navCtrl.canGoBack()) {
       this.navCtrl.pop();
     }
   }
 
-  openMap(){
-    let options: LaunchNavigatorOptions = {
-      start: 'London, ON',
-    };
-    this.launchNavigator.navigate('Toronto, ON', options)
-      .then(
-        success => console.log('Launched navigator'),
-        error => console.log('Error launching navigator', error)
-      );
+  openMap() {
+    if (this.plt.is('core')) {
+      window.open('https://maps.google.com/maps?saddr=toronto&daddr=ottawa', '_system');
+    } else {
+      let options: LaunchNavigatorOptions = {
+        start: 'London, ON',
+      };
+      this.launchNavigator.navigate('Toronto, ON', options)
+        .then(
+          success => console.log('Launched navigator'),
+          error => console.log('Error launching navigator', error)
+        );
+    }
+  }
+
+  addToNativeCalendar() {
   }
 
 }
